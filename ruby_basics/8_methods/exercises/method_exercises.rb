9fe9804e40c75757d5bed49c19087942550fb233
raliwalas <rachel.aliwalas52@gmail.com>
2023-11-19 19:56:22 -0500

    methods exercises

--- ruby_basics/8_methods/exercises/method_exercises.rb
+++ ruby_basics/8_methods/exercises/method_exercises.rb
@@ -7,11 +7,22 @@
 # return value: the number's ASCII character (https://www.ascii-code.com/)
 # hint: use Integer#chr
 
+def ascii_translator(number)
+
+    number.chr
+
+end
 
 # method name: #common_sports
 # parameters: current_sports and favorite_sports (both arrays)
 # return value: an array containing items in both arrays
 # hint: use Array#intersection
+
+def common_sports(current_sports, favorite_sports)
+
+    current_sports.intersection(favorite_sports)
+
+end
 
 
 # method name: #alphabetical_list
@@ -19,11 +30,22 @@
 # return value: games, alphabetically sorted and duplicates removed
 # hint: chain Array#sort and Array#uniq together
 
+def alphabetical_list(games)
+
+    games.sort.uniq()
+
+end
+
 
 # method name: #lucky_number
 # parameter: number (an integer) with default value of 7
 # return value: a string "Today's lucky number is <number>"
 
+def lucky_number(num=7)
+    
+    return "Today's lucky number is #{num}"
+
+end
 
 # method name: #ascii_code
 # parameter: character (a string)
@@ -31,6 +53,15 @@
 # explicit return value: 'Input Error' if character's length does not equal 1
 # hint: use String#ord
 
+def ascii_code(string)
+
+    if (string.length != 1)
+        return "Input Error"
+    else
+        string.ord
+    end
+
+end
 
 # method name: #pet_pun
 # parameter: animal (a string)
@@ -41,8 +72,32 @@
 # hint: use puts
 
 
+def pet_pun(animal)
+
+    # to ensure that the if/else matches the animal's case
+    animal.downcase!
+
+    if animal == "cat"
+        puts "Cats are purr-fect!"
+    elsif animal == "dog"
+        puts "Dogs are paw-some!"
+    else
+        puts "I think #{animal}s have pet-tential!"
+    end
+
+    #puts instead of return bc puts returns nil
+
+end
+
 # method name: #twenty_first_century?
 # parameter: year (an integer)
 # return value: true if the year is between 2001 - 2100, otherwise return false
 # hint: use Comparable#between?
 
+
+def twenty_first_century?(year)
+
+    year.between?(2001, 2100)
+
+end
+    
